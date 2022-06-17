0b8c0dd07be43cb7af3e6b215ea4ce7f2860d4eb
Mike Alessi <72767353+Malessi5@users.noreply.github.com>
2022-06-17 15:41:21 -0400

    fix order of cart token searching

--- add_to_cart.js
+++ add_to_cart.js
@@ -46,30 +46,35 @@ function sendPostReq(items, checkoutUrl) {
   });
 }
 
-function reChargeProcessCart() {
+function getCookieFromToken() {
   function get_cookie(name) {
     return (document.cookie.match("(^|; )" + name + "=([^;]*)") || 0)[2];
   }
 
-  let token;
-  let foundToken = false;
+  let token = "";
   let count = 0;
 
-  let cookieCheck = setInterval(() => {
+  while (!token && count < 10) {
     token = get_cookie("cart");
     count++;
-    if (token || count >= 5) {
-      foundToken = true;
-      console.log("onion token =", token);
-      clearInterval(cookieCheck);
-    }
-  }, 1000);
-
-  if (!foundToken) {
-    token = getCartToken();
   }
+  console.log("cookie token", token);
+  return token;
+}
 
-  return generateCheckoutLink(token);
+function getCartToken() {
+  fetch("/cart.js")
+    .then(function (res) {
+      return res.json();
+    })
+    .then(
+      function (cart) {
+        console.log("bidge token =", cart.token);
+        return cart.token;
+      }.catch(function (e) {
+        console.error("No cart token found", e);
+      })
+    );
 }
 
 function generateCheckoutLink(token) {
@@ -87,22 +92,26 @@ function generateCheckoutLink(token) {
   return checkout_url;
 }
 
-function getCartToken() {
-  fetch("/cart.js")
-    .then(function (res) {
-      return res.json();
-    })
-    .then(function (cart) {
-      console.log("bidge token =", cart.token);
-      return cart.token;
-    });
+function getToken() {
+  let cookieToken = getCookieFromToken();
+  if (cookieToken) {
+    return cookieToken;
+  } else {
+    return getCartToken();
+  }
 }
 
 (function init() {
-  const checkoutUrl = reChargeProcessCart();
   const data = createObjectFromParams(window.location.href);
+  const token = getToken();
 
-  if (data && checkoutUrl) {
-    sendPostReq(data, checkoutUrl);
+  if (token) {
+    const checkoutUrl = generateCheckoutLink(token);
+
+    if (data && checkoutUrl) {
+      sendPostReq(data, checkoutUrl);
+    }
+  } else {
+    console.log("No cart onion found");
   }
 })();
