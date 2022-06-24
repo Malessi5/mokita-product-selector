6813b232b45e44f5c0e8ba0b478d9dc99e63d069
Mike Alessi <72767353+Malessi5@users.noreply.github.com>
2022-06-24 18:34:49 -0400

    update order.js

--- order.js
+++ order.js
@@ -3,11 +3,13 @@ const priceData = { 4156236: 56, 4156334: 96, 4156347: 168 };
 function populateSummary(data) {
   const { desc, set, price, origPrice, bump, bumpPrice } = data;
   jQuery(".productdesc p:first-child").text(desc);
-  jQuery(".pack.martopbottom span").text(`Exclusive Offer - ${set} Pack`);
+  jQuery(".pack.martopbottom span").text(
+    "Exclusive Offer - " + set.toString() + "Pack"
+  );
   jQuery("p.productprice").text(price);
   jQuery("p.oldprice").text(origPrice);
   let savings = Math.round(((price - origPrice) / origPrice) * 100);
-  jQuery("p.savings").text(`${savings}% SAVINGS APPLIED`);
+  jQuery("p.savings").text("$" + savings.toString() + "% SAVINGS APPLIED");
 
   let totals = jQuery(".pull-right.elOrderProductOptinPrice.product-price");
   let totalCost = bump ? price + bumpPrice : price;
@@ -20,17 +22,17 @@ function getParamObject(urlStr) {
   const urlParams = new URLSearchParams(urlStr);
   let id = urlParams.get("pid");
   let addon = urlParams.get("addon");
-
+  console.log("pid", id);
   let product = {
     desc: "Microsphere Shampoo & Conditioner",
     bump: addon,
     bumpPrice: 0,
-    origPrice: `$${priceData[id].toFixed(2).toString()}`,
+    origPrice: priceData[id].toFixed(2),
   };
 
   jQuery(".pull-left.elOrderProductOptinProductName input").each((i, e) => {
     if (jQuery(e).attr("value") === id) {
-      product.price = jQuery(e).attr("data-product-amount");
+      product.price = parseInt(jQuery(e).attr("data-product-amount"));
       product.set = parseInt(i + 1);
       jQuery(e).prop("checked", true);
     }
